96c4e22267226c155a7b6fd90d54413166ed8753
Tadres <nicolas.tadres@edu.esiee.fr>
2022-06-03 14:49:52 +0200

    Class Solver

---
+++ src/lib/cubes/Solver.ts
@@ -0,0 +1,10 @@
+import { ThreeByThree } from './ThreeByThree';
+
+export class Solver {
+
+    private _cube : ThreeByThree
+
+    public constructor() {
+        this._cube = new ThreeByThree();
+    }
+}
