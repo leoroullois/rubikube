66cc6bba6ad6527c8711f5f5da6572dc4ffeb384
leoroullois <roullois.leo@gmail.com>
2022-06-13 23:49:54 +0200

    feat: timer

--- src/pages/timer.tsx
+++ src/pages/timer.tsx
@@ -1,134 +1,157 @@
 import Head from "next/head";
-import { MouseEventHandler, useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useCallback, useEffect, useState } from "react";
 
-import CubePattern from "@components/cube-pattern";
 import Wrapper from "@components/wrapper";
-import Scramble from "@lib/cubes/Scramble";
 import { ThreeByThree } from "@lib/cubes/ThreeByThree";
-import { selectTimer } from "@store/selectors";
-import { setCubeArray } from "@store/slices/timer";
-import { Moves, Rotations } from "@lib/cubes/Moves";
-import { HexColor } from "@lib/cubes/types";
-import Genetic from "@lib/cubes/Genetic";
-import Cube from "@lib/cubes/Cube";
+import CubePattern from "@components/cube-pattern";
 
 const Timer = () => {
-   const dispatch = useDispatch();
-   const { cubeArray } = useSelector(selectTimer);
-   const [scramble, setScramble] = useState("");
-   
-   const handleScramble: MouseEventHandler = (e) => {
-      const cube = new ThreeByThree();
+  const [mounted, setMounted] = useState(false);
 
-      const myScramble = new Scramble();
+  const [ms, setMs] = useState(0);
+  const [isActive, setIsActive] = useState(false);
+  const [times, setTimes] = useState<number[]>([]);
 
-      cube.move(myScramble.scramble);
-      dispatch(setCubeArray(cube.cubeArray));
+  const [cube] = useState(new ThreeByThree());
+  const [cubeArray, setCubeArray] = useState(cube.cubeArray);
+  const [scramble, setScramble] = useState(cube.scramble);
 
-      setScramble(myScramble.scramble);
-   };
+  const resetTimer = () => {
+    setMs(0);
+    setIsActive(false);
+  };
+  const startTimer = () => {
+    setIsActive(true);
+  };
+  const stopTimer = () => {
+    setIsActive(false);
+  };
 
-   const handleRotate = (rotation: Rotations) => {
-      const cube = new ThreeByThree(cubeArray);
-      cube.move(rotation);
-      dispatch(setCubeArray(cube.cubeArray));
-      setScramble((scramble) => scramble + rotation + " ");
-   };
+  const getDecimals = (ms: number) => {
+    return Math.floor(ms / 10) % 100;
+  };
+  const getSeconds = (ms: number) => Math.floor(ms / 1000);
+  const getMinutes = (ms: number) => Math.floor(getSeconds(ms) / 60);
 
-   const handleMove = (move: string) => {
-      const cube = new ThreeByThree();
-      console.log("🚨 [MOVE]", move);
-      cube.move(scramble + move);
-      dispatch(setCubeArray(cube.cubeArray));
-      setScramble((scramble) => scramble + move + " ");
-   };
+  const getAo = (nb: number, times: number[]): string => {
+    if (times.length < nb) return "--";
+    const aoTimes = [...times].slice(-1 * nb).sort((a, b) => a - b);
+    aoTimes.pop();
+    aoTimes.shift();
+    const sum = aoTimes.reduce((acc, curr) => acc + curr, 0);
+    return String(Math.round(sum / aoTimes.length));
+  };
 
-   const handleReset = () => {
-      const cube = new ThreeByThree();
-      dispatch(setCubeArray(cube.cubeArray));
-      setScramble("");
-   };
+  useEffect(() => {
+    let interval: ReturnType<typeof setInterval> | null = null;
+    if (isActive) {
+      interval = setInterval(() => {
+        setMs((ms) => ms + 10);
+      }, 10);
+    }
+    return () => {
+      !!interval && clearInterval(interval);
+    };
+  }, [isActive]);
 
-   const handleSolve = () => {
-      const genetic = new Genetic(scramble);
-      genetic.solve();
-   };
+  const [start, setStart] = useState(true);
+  const [lastIsKeyDown, setLastIsKeyDown] = useState(false);
+  const handleKeyDown = useCallback(
+    (e: KeyboardEvent) => {
+      e.preventDefault();
+      e.stopPropagation();
+      console.log("keydown", e.key, start);
+      if (!lastIsKeyDown) {
+        if (isActive) {
+          setStart(false);
+        } else {
+          setStart(true);
+        }
 
-   useEffect(() => {
-      console.table(Cube.getColorArray(cubeArray));
+        // ? stop timer
+        if (isActive) {
+          times.push(ms);
+          stopTimer();
+        }
+        setLastIsKeyDown(true);
+      }
+    },
+    [isActive, lastIsKeyDown, ms, start, times]
+  );
 
-      const cube = new ThreeByThree();
-      cube.move("M");
-      console.log(cube.cubeArray);
-      console.log(cube.getRubik());
-   }, [cubeArray]);
+  const handleKeyUp = useCallback(
+    (e: KeyboardEvent) => {
+      e.preventDefault();
+      e.stopPropagation();
+      console.log("keyup", e.key, start);
+      if (!isActive && e.key === " " && start) {
+        setMs(0);
+        startTimer();
+      }
+      setLastIsKeyDown(false);
+    },
+    [isActive, start]
+  );
 
-   return (
-      <>
-         <Head>
-            <title>Timer</title>
-         </Head>
-         <main className='flex flex-col'>
-            <Wrapper className='flex flex-col gap-y-5'>
-               <h1 className='text-3xl text-center my-5 font-bold'>Timer</h1>
-               <CubePattern cubeArray={cubeArray} />
-               <>
-                  {scramble && <p>Scramble : {scramble.replace(/i/g, "'")}</p>}
-               </>
-               <button
-                  className='flex justify-center p-2 w-32 rounded text-gray-900 bg-green-400 hover:bg-green-500'
-                  onClick={handleScramble}
-               >
-                  Scramble
-               </button>
-               <button
-                  className='flex justify-center p-2 w-32 rounded text-gray-900 bg-blue-400 hover:bg-blue-500'
-                  onClick={handleSolve}
-               >
-                  Solve
-               </button>
-               <button
-                  onClick={handleReset}
-                  className='flex justify-center p-2 w-32 rounded text-gray-900 bg-red-400 hover:bg-red-500'
-               >
-                  Reset
-               </button>
-               <div className='flex gap-x-5'>
-                  <section className='flex flex-col gap-y-2'>
-                     {Object.values(Rotations).map((rotation, i) => {
-                        return (
-                           <button
-                              onClick={() => handleRotate(rotation)}
-                              key={i}
-                              className='flex justify-center p-2 w-32 rounded text-gray-900 bg-indigo-400 hover:bg-indigo-500'
-                           >
-                              Rotate {rotation}
-                           </button>
-                        );
-                     })}
-                  </section>
-                  <section className='flex flex-row w-full flex-wrap gap-x-2'>
-                     {Object.values(Moves).map((move, i) => {
-                        return (
-                           <button
-                              onClick={() =>
-                                 handleMove(move.replace(/i/g, "'"))
-                              }
-                              key={i}
-                              className='flex justify-center p-2 w-32 h-10 rounded text-gray-900 bg-indigo-400 hover:bg-indigo-500'
-                           >
-                              Rotate {move}
-                           </button>
-                        );
-                     })}
-                  </section>
-               </div>
-            </Wrapper>
-         </main>
-      </>
-   );
+  const updateCubeState = useCallback((cube: ThreeByThree) => {
+    setCubeArray(cube.cubeArray);
+    setScramble(cube.scramble);
+  }, []);
+
+  useEffect(() => {
+    document.addEventListener("keyup", handleKeyUp);
+    return () => {
+      document.removeEventListener("keyup", handleKeyUp);
+    };
+  }, [handleKeyUp]);
+
+  useEffect(() => {
+    document.addEventListener("keydown", handleKeyDown);
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [handleKeyDown]);
+
+  useEffect(() => {
+    cube.randomlyScrambleCube();
+    updateCubeState(cube);
+    setMounted(true);
+  }, [cube, updateCubeState]);
+
+  const formatTime = (ms: number | string) => {
+    const nb = Number(ms);
+    if (Number.isNaN(nb)) {
+      return "--:--";
+    }
+    return `${getSeconds(nb).toString().padStart(2, "0")}:${getDecimals(nb)
+      .toString()
+      .padEnd(2, "0")}`;
+  };
+  return (
+    <>
+      <Head>
+        <title>Timer</title>
+      </Head>
+      <main className="flex flex-col">
+        <Wrapper className="flex flex-col gap-y-5">
+          <h1 className="text-3xl text-center my-5 font-bold">Timer</h1>
+          <h2 className="text-xl text-center font-bold">{scramble}</h2>
+          <h2 className="text-5xl text-center font-mono font-medium">
+            {formatTime(ms)}
+          </h2>
+          <button onClick={startTimer}>Start</button>
+          <button onClick={stopTimer}>Stop</button>
+          <button onClick={resetTimer}>Reset</button>
+          <>{!!mounted && <CubePattern cubeArray={cubeArray} />}</>
+          <ul>
+            <li>AO5 : {formatTime(getAo(5, times))}</li>
+            <li>AO12 : {formatTime(getAo(12, times))}</li>
+            <li>AO50 : {formatTime(getAo(50, times))}</li>
+          </ul>
+        </Wrapper>
+      </main>
+    </>
+  );
 };
 
 export default Timer;
-
